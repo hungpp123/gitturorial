7b10cf3f0a37017d3644b12eadd1a9719262bf42
hungpp123 <nguyendinhhunghn96@gmail.com>
2025-11-21 08:17:21 +0700

    add class app from main class

---
+++ src/main/java/com/hungnd62/testmaven/App.java
@@ -0,0 +1,8 @@
+package com.hungnd62.testmaven;
+
+public class App {
+    //write a main method that prints "Hello, World!"
+    public static void main(String[] args) {
+        System.out.println("Hello from main branch");
+    }
+}
